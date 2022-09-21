313466c9f3aab45f026f4eda5add4d7ee56260d0
Aman <amanvermaspace@gmail.com>
2022-09-21 11:53:51 +0530

    tried serving images, application not able to

--- server.js
+++ server.js
@@ -16,43 +16,38 @@ app.listen(port, () => {
 })
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + "/index.html");
-    // before sending the result back to client we use the https module
+
     const url = "https://api.openweathermap.org/data/2.5/weather";
     const city = "london";
     const units = "imperial";
     const appid = "d6cb3f891806944d6df035d7343904db";
     https.get(`${url}?q=${city}&units=${units}&mode=json&appid=${appid}`, (response) => {
         console.log(response.statusCode);
-
-        // logging the data coming through the API
-        response.on('data', function(data){
-            // console.log(data);
-            // to get the data in an already parsed format, we either use the JSON.parse(data) or we use the process.stdout.write(data).  But this prints the stringified data
+        
+        response.on("data", function(data){
+            // console.log(JSON.parse(data));
+            
             let weatherDataObj = JSON.parse(data);
             let temp = weatherDataObj.main.temp;
             let icon = weatherDataObj.weather[0].icon;
+            const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
             let description = weatherDataObj.weather[0].description;
             let pressure = weatherDataObj.main.pressure;
             let humidity = weatherDataObj.main.humidity;
 
-            const dataObj = {
-                temp: temp,
-                icon: icon,
-                description: description,
-                pressure: pressure,
-                humidity: humidity
-            }
-            console.log(dataObj.temp);
-
-            // To convert the data into a string
-            // console.log(JSON.stringify());
+            // after everything has been written, use res.send() to send the information all together.
+            // ** Note : We can only send string data using res.send() or res.write(), we cannot send integers, so wither send it as an object, or stringify it
+            res.write(JSON.stringify(temp));
+            res.write(description);
+            res.write(`<img src=${imageUrl} />`);
+            res.write("<img src=" + imageUrl + ">");
+            res.send();
         })
-        // logging the error if any
-        response.on('error', function(e){
-            console.log(e);
-        })
+        
+    }).on("error", (e) => {
+        console.log(e);
     })
+    // res.sendFile(__dirname + "/index.html");
 })
 
 app.post('/', (req,res) => {
